84b08916f24dc8a787004388c25a47704f5dfcbb
cdx_yoop <2843664069@qq.com>
2025-01-14 22:07:40 +0800

    run baseline

--- script/audio_tool.py
+++ script/audio_tool.py
@@ -1,17 +1,22 @@
 import ffmpeg
 import subprocess
 
-# def audio_extract(input, output):
-# 	ffmpeg.input(input, vn=None).output(output).run()
+
+def audio_extract(input, output):
+	print(input)
+	print(output)
+	with open(input, 'rb') as f:
+		print(f)
+	ffmpeg.input(input, vn=None).output(output).run()
 
 # 解决中文路径ffmpeg无法运行的问题
-def audio_extract(input_path, output_path):
-	command = [
-		'ffmpeg', 
-		'-i', 'pipe:0',  # 从stdin读取输入
-		'-vn',          # 仅提取音频
-		output_path     # 输出文件路径
-	]
-
-	with open(input_path, 'rb') as f:
-		subprocess.run(command, input=f.read())
+# def audio_extract(input_path, output_path):
+# 	command = [
+# 		'ffmpeg',
+# 		'-i', 'pipe:0',  # 从stdin读取输入
+# 		'-vn',          # 仅提取音频
+# 		output_path     # 输出文件路径
+# 	]
+#
+# 	with open(input_path, 'rb') as f:
+# 		subprocess.run(command, input=f.read())
